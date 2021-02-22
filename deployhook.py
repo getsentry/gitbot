b384a0b018fd0c5004f0a769d1afa3867e0a3795
Armin Ronacher <armin.ronacher@active-4.com>
2021-02-22 11:39:43 +0100

    Accept entire key from envvar

--- deployhook.py
+++ deployhook.py
@@ -20,11 +20,7 @@ DEPLOY_REPO = 'git@github.com:getsentry/getsentry'
 DEPLOY_BRANCH = 'master'
 COMMITTER_NAME = 'Sentry Bot'
 COMMITTER_EMAIL = 'bot@getsentry.com'
-SSH_KEY = '''
------BEGIN OPENSSH PRIVATE KEY-----
-%s
------END OPENSSH PRIVATE KEY-----
-''' % os.environ['DEPLOY_SSH_KEY']
+SSH_KEY = os.environ['DEPLOY_SSH_KEY']
 
 PLUGIN_REPOS = [
     'getsentry/sentry-plugins',
