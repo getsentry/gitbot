3505f46f36adb2bfa483097b138962a75d7c9f0f
Armen <armenzg@sentry.io>
2021-05-11 14:47:47 -0400

    Debugging

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,8 @@
 # This Docker entrypoint guarantees that a private key will be generated
 set -e
 
+env
+ls -l /app
 # Try to create the private key
 if [ ! -f /app/private_ssh_key ] && [ -n "${DEPLOY_SSH_KEY}" ]; then
     # Writing an SSH key via UNIX tools was tricky
@@ -18,5 +20,8 @@ fi
 #     echo -e "The container needs a private key and this is created via DEPLOY_SSH_KEY (see docs for details)" >&2
 #     exit 1
 # fi
+ls -l /app
+cat /app/private_ssh_key || echo "Meh"
+ssh -T git@github.com -i /app/private_ssh_key
 
 exec "$@"
