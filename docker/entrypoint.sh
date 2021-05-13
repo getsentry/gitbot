b734c86d7efdba965693664d03d32db76c59130a
Armen Zambrano Gasparnian <armenzg@sentry.io>
2021-05-13 15:44:39 -0400

    Undo removing entrypoint from docker-compose

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This Docker entrypoint guarantees that a private key will be generated
 set -e
-set -x
+
 # Try to create the private key
 if [ ! -f /app/private_ssh_key ] && [ -n "${DEPLOY_SSH_KEY}" ]; then
     # Writing an SSH key via UNIX tools was tricky
@@ -14,5 +14,11 @@ if [ ! -f /app/private_ssh_key ]; then
     echo -e "The container needs a private key and this is created via DEPLOY_SSH_KEY (see docs for details)" >&2
     exit 1
 fi
+
+# If the key has a passphrase this will fail
+# This verifies that the key is in the right format, however, even on success it exits with non-success
+# XXX: Comment out temporarily
+# ssh -T git@github.com -v -i /app/private_ssh_key ||
+#     [ ${?} == 255 ] && echo "The key is an improper format. Look into it." && rm /app/private_ssh_key && exit 1
 
 exec "$@"
