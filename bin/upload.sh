e145bf62bea6f27df9d7f43719ea422416adf386
Armen Zambrano G <armenzg@sentry.io>
2021-05-25 09:44:44 -0400

    Last changes

--- bin/upload.sh
+++ bin/upload.sh
@@ -6,9 +6,6 @@ NAME=sentry-deploy-sync-hook
 PROJECT=sentry-dev-tooling
 IMAGE=gcr.io/${PROJECT}/${NAME}
 
-# docker build --tag ${NAME}:latest .
-# docker tag ${NAME} ${IMAGE}
-# docker push ${IMAGE}
 gcloud builds submit \
     --tag us.gcr.io/sentry-dev-tooling/sentry-deploy-sync-hook \
     --project=sentry-dev-tooling
