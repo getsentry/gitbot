726e907404514654565b69f4d829a52219efd202
Armen Zambrano G <armenzg@sentry.io>
2021-05-26 10:25:37 -0400

    WIP

--- test_service.py
+++ test_service.py
@@ -1,9 +1,16 @@
+import os
 import requests
 
 # url = "https://sentry-deploy-sync-hook-dwunkkvj6a-uc.a.run.app"
 url = "http://0.0.0.0:8080"
-myobj = {"somekey": "somevalue"}
+GITHUB_WEBHOOK_SECRET = os.environ.get("GITHUB_WEBHOOK_SECRET")
+myobj = {
+    "somekey": "somevalue",
+}
+headers = {
+    "X-Hub-Signature": GITHUB_WEBHOOK_SECRET,
+}
 
-x = requests.post(url, data=myobj)
+x = requests.post(url, headers=headers, data=myobj)
 
 print(x.text)
