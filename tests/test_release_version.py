a881f90dc6b5b04b3485ea8b72885aced37a558f
Armen <armenzg@sentry.io>
2021-08-20 14:54:50 -0400

    Change test

--- tests/test_release_version.py
+++ tests/test_release_version.py
@@ -14,16 +14,16 @@ def test_release_version(tmpdir):
     run(f"git clone --depth 10 {CHECKOUT_ROOT_PATH} {new_checkout}")
     run("git log -5 --oneline", cwd=new_checkout)
     run("git checkout 68fd0d2aa1dbdc72af79b85f7d65a81fb4723dbd", cwd=new_checkout)
-    version = run("./release_version.sh", cwd=new_checkout).stdout
+    release = run("./release_version.sh", cwd=new_checkout).stdout
     # XXX: Adjust the version once it lands on master
-    assert version in (
-        "2021.8.20+102918.armenzg-semver.68fd0d2",
-        "2021.8.20+102918.HEAD.68fd0d2",
+    assert release in (
+        "gitbot@2021.8.20+102918.armenzg-semver.68fd0d2",
+        "gitbot@2021.8.20+102918.HEAD.68fd0d2",
     )
     # The release version should be idempotent
     time.sleep(3)
-    version = run("./release_version.sh", cwd=new_checkout).stdout
-    assert version in (
-        "2021.8.20+102918.armenzg-semver.68fd0d2",
-        "2021.8.20+102918.HEAD.68fd0d2",
+    release = run("./release_version.sh", cwd=new_checkout).stdout
+    assert release in (
+        "gitbot@2021.8.20+102918.armenzg-semver.68fd0d2",
+        "gitbot@2021.8.20+102918.HEAD.68fd0d2",
     )
