92b98f05cb4a2a458f68084f9d785a8f5e397d44
Armen Zambrano G <armenzg@sentry.io>
2021-09-20 14:54:35 -0400

    fix: Do not try to preserve double quotes (#75)
    Add tests for bump_version.

--- gitbot/deployhook.py
+++ gitbot/deployhook.py
@@ -2,7 +2,6 @@ import hmac
 import hashlib
 import logging
 import os
-import tempfile
 from operator import itemgetter
 
 import sentry_sdk
@@ -84,43 +83,6 @@ def respond(data, status_code):
     if status_code != 200:
         sentry_sdk.capture_message(data["reason"], "fatal")
     return jsonify(data), status_code
-
-
-def bump_version(branch, ref_sha, author=None):
-    repo_root = tempfile.mkdtemp()
-
-    # The branch has to be created manually in getsentry/getsentry!
-    try:
-        run(
-            f"git clone --depth 1 -b {branch} {GETSENTRY_REPO_URL} {repo_root}",
-            cwd=repo_root,
-        )
-    except CommandError:
-        return False, "Cannot clone branch {} from {}.".format(branch, GETSENTRY_REPO)
-
-    run(f"git config user.name {COMMITTER_NAME}", cwd=repo_root)
-    run(f"git config user.email {COMMITTER_EMAIL}", cwd=repo_root)
-
-    command = bump_command(ref_sha, author)
-    run(command, cwd=repo_root)
-
-    if DRY_RUN:
-        push_cmd = f"git push origin --dry-run {branch}"
-    else:
-        push_cmd = f"git push origin {branch}"
-    successful_push = False
-    for _ in range(5):
-        try:
-            run(push_cmd, cwd=repo_root)
-            successful_push = True
-            break
-        except CommandError:
-            run(f"git pull --rebase origin {branch}", cwd=repo_root)
-
-    if not successful_push:
-        return False, "Failed to push."
-    else:
-        return True, f"Executed: {command}"
 
 
 # Github's UI looks really bad when most responses are 400
--- tests/test_version_bump.py
+++ tests/test_version_bump.py
@@ -1,4 +1,6 @@
-from gitbot.lib import bump_command, extract_author
+import os
+
+from gitbot.lib import bump_command, bump_version, extract_author
 
 event = {
     "head_commit": {
@@ -10,17 +12,26 @@ event = {
     },
 }
 
-# fmt: off
-expected_author = (
-    'Aniket Das \"Tekky <85517732+AniketDas-Tekky@users.noreply.github.com>'
-)
-# fmt: on
+expected_author = "Aniket Das Tekky <85517732+AniketDas-Tekky@users.noreply.github.com>"
 
 
 def test_bump_command():
     assert bump_command("master", extract_author(event)) == [
-        "bin/bump-sentry",
+        "tests/bin/bump-sentry",
         "master",
         "--author",
         expected_author,
     ]
+
+
+def test_bump_version():
+    # This will checkout gitbot in a tempdir and try calling bin/bump-sentry
+    bump_version(
+        "master",
+        # Any valid sha is good enough
+        "9962ffff3d0b1973fb05e16cd6a3328c5ecb1401",
+        extract_author(event),
+        # Path to gitbot checkout (i.e. Git top level dir)
+        url=os.path.dirname(__file__).rsplit("/", 1)[0],
+        dry_run=True,
+    )
