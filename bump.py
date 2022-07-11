55d55f71e7bbad6c0fdf2f9aa7e6a24d42ad3bc3
Joshua Li <joshuarli98@gmail.com>
2022-07-11 16:12:42 -0700

    fix -Xtheirs merge

--- bump.py
+++ bump.py
@@ -15,12 +15,9 @@ from gitbot.lib import bump_version, run
 logging.getLogger().setLevel("DEBUG")
 logging.basicConfig()
 
-# TODO: replace b1ad2facd059465b344beb075037ecad0aa467bc with
+# TODO: replace 4020db731c2d0be72b6df589e2e97859b92ad824 with
 #       merge sha of https://github.com/getsentry/sentry/pull/34879
-_ref_sha="b1ad2facd059465b344beb075037ecad0aa467bc"
-
-# TODO: also replace 4020db731c2d0be72b6df589e2e97859b92ad824
-#       with another sentry sha ahead of b1ad2facd
+_ref_sha = "4020db731c2d0be72b6df589e2e97859b92ad824"
 
 
 def validate_bump(result: bool, text: str, temp_checkout: str) -> None:
@@ -29,14 +26,14 @@ def validate_bump(result: bool, text: str, temp_checkout: str) -> None:
     assert text == f"Executed: bin/bump-sentry {_ref_sha}"
     execution = run("git show -s --oneline", temp_checkout)
     assert (
-        "getsentry/sentry@ccc86db8a6a2541b5786f76e8461f587a8adca20" in execution.stdout
+        f"getsentry/sentry@{_ref_sha}" in execution.stdout
     ), execution.stdout
-    execution = run("git grep ccc86db8a6a2541b5786f76e8461f587a8adca20", tmpdir)
+    execution = run(f"git grep {_ref_sha}", temp_checkout)
     split_lines = execution.stdout.splitlines()
     assert split_lines == [
-        "cloudbuild.yaml:            '--build-arg', 'SENTRY_VERSION_SHA=ccc86db8a6a2541b5786f76e8461f587a8adca20',",
-        "docker/frontend_cloudbuild.yaml:      '--build-arg', 'SENTRY_VERSION_SHA=ccc86db8a6a2541b5786f76e8461f587a8adca20',",
-        "sentry-version:ccc86db8a6a2541b5786f76e8461f587a8adca20",
+        f"cloudbuild.yaml:            '--build-arg', 'SENTRY_VERSION_SHA={_ref_sha}',",
+        f"docker/frontend_cloudbuild.yaml:      '--build-arg', 'SENTRY_VERSION_SHA={_ref_sha}',",
+        f"sentry-version:{_ref_sha}",
     ], split_lines
 
 
@@ -52,8 +49,14 @@ def main(argv: Sequence[str] | None = None) -> int:
         required=True,
         help="Path to getsentry checkout.",
     )
+    parser.add_argument(
+        "--sentry-path",
+        type=str,
+        help="Path to sentry checkout.",
+    )
     args = parser.parse_args(argv)
     args.getsentry_path = os.path.abspath(args.getsentry_path)
+
     tmpdir = mkdtemp()
     temp_checkout = f"{tmpdir}/getsentry"
     try:
@@ -61,17 +64,17 @@ def main(argv: Sequence[str] | None = None) -> int:
         # We make a soft clone of it into a tempdir and then try to bump
         result, text = bump_version(
             branch=args.branch,
-            ref_sha="ccc86db8a6a2541b5786f76e8461f587a8adca20",  # Random sha from Sentry repo
+            ref_sha=_ref_sha,  # Random sha from Sentry repo
             url=args.getsentry_path,  # It will soft clone
             dry_run=True,  # This will prevent trying to push
             temp_checkout=temp_checkout,
-            sentry_path=sentry_path,
+            sentry_path=args.sentry_path,
         )
         validate_bump(result, text, temp_checkout)
     finally:
         # This undoes what bump version did
-        run("git config --unset user.name", cwd=args.getsentry_path, raise_error=False)
-        run("git config --unset user.email", cwd=args.getsentry_path, raise_error=False)
+        run("git config --unset user.name", cwd=temp_checkout, raise_error=False)
+        run("git config --unset user.email", cwd=temp_checkout, raise_error=False)
         shutil.rmtree(tmpdir)
     return 0
 
