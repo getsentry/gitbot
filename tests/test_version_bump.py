4c329a55bdc8b2700f4e6206e6f6f36a1c5eabd2
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2022-09-01 15:47:30 -0400

    remove some dead code (#109)

--- tests/test_version_bump.py
+++ tests/test_version_bump.py
@@ -3,7 +3,6 @@ from unittest.mock import patch
 from gitbot.lib import (
     bump_command,
     bump_sentry_path,
-    extract_author,
 )
 
 event = {
@@ -15,7 +14,6 @@ event = {
         },
     },
 }
-expected_author = "Aniket Das Tekky <85517732+AniketDas-Tekky@users.noreply.github.com>"
 tests_bump_sentry_path = "tests/bin/bump-sentry"
 
 
@@ -27,18 +25,4 @@ def test_different_bump_sentry_path_with_env(monkeypatch):
 @patch("gitbot.lib.bump_sentry_path")
 def test_bump_command(mock_bump_path):
     mock_bump_path.return_value = tests_bump_sentry_path
-    assert bump_command(ref_sha="foo", author=extract_author(event)) == [
-        tests_bump_sentry_path,
-        "foo",
-        "--author",
-        expected_author,
-    ]
-
-
-@patch("gitbot.lib.bump_sentry_path")
-def test_bump_command_no_author(mock_bump_path):
-    mock_bump_path.return_value = tests_bump_sentry_path
-    assert bump_command(ref_sha="foo") == [
-        tests_bump_sentry_path,
-        "foo",
-    ]
+    assert bump_command(ref_sha="foo") == [tests_bump_sentry_path, "foo"]
